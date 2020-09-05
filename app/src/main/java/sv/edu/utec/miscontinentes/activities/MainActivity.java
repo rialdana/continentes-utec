0f6e6c06dec71c11b463e403b2a10c365e1cf560
Ezophagothomia <43972877+Ezophagothomia@users.noreply.github.com>
2020-09-04 22:02:13 -0600

    Labores completadas

--- app/src/main/java/sv/edu/utec/miscontinentes/activities/MainActivity.java
+++ app/src/main/java/sv/edu/utec/miscontinentes/activities/MainActivity.java
@@ -2,15 +2,29 @@ package sv.edu.utec.miscontinentes.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import sv.edu.utec.miscontinentes.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIngresar = findViewById(R.id.btnIngresar);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ContinentsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
